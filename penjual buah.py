b397b87d78bbe3941811b6c72fb62dc2e5bd67c8
pokemon2808 <dedeaqzhar12@gmail.com>
2022-10-20 22:31:01 +0800

    first commit

---
+++ penjual buah.py
@@ -0,0 +1,4 @@
+basah=850
+kering=75
+jumlah=25
+print =("berat masing masing karung =",(basah-kering)/jumlah,'kg')
---
+++ tipe data.py
@@ -0,0 +1,12 @@
+nama = "muh arif"
+umur = 20
+tinggi = 1.75
+pengusaha = True
+print ("nama : ",nama)
+print (type (nama))
+print("umur : ",umur)
+print(type (umur))
+print ("tinggi : ",tinggi)
+print (type (tinggi))
+print("pengusaha : ",pengusaha)
+print(type (pengusaha))
